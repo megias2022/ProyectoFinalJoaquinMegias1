e88f6d43123b101f7799454f27332cbfc7998cee
Joaquin <megiasmultimedia@gmail.com>
2023-01-10 21:43:12 -0300

    Persiste problema de carga de Post

--- final_app/models.py
+++ final_app/models.py
@@ -6,11 +6,11 @@ class Post(models.Model):
     sub_titulo = models.CharField(max_length=100)
     texto = models.TextField(max_length=3000)
     publicado_el = models.DateField(auto_now_add=True)
-    imagen = models.ImageField(upload_to="posteos", null="True", blank=True)    
+    imagen = models.ImageField(upload_to="posteos", null=True, blank=True)    
 
 class Avatar(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="avatar")
-    imagen = models.ImageField(upload_to="avatares", null="True", blank=True)
+    imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
     
 class Mensaje(models.Model):
     email = models.EmailField()
